d1e98f3b1c11d5e5b8f95adad155069beb465675
taa00008 <taa00008@mix.wvu.edu>
2024-03-03 11:13:20 -0500

    Added Card and Navbar javascript files
    added the two new javascript files and imported bootstrap into Index.js, also imported the two new files into App.js, edited the App.css for formating purpose

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import './Navbar.js';
+import './Card.js';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>CS 230L</h1>
-      <h2>Section - 001</h2>
+  const cardContent = (
+    <div>
+      <h2>CS 230L</h2>
+      <h3>Section - 001</h3>
       <p>WVU ID: 800365874</p>
       <p>Hi I am Tyler Ashmore</p>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Card title="Card 01" content={cardContent} />
     </div>
   );
 }
