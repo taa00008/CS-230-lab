c5950f576a459e81c892137901ed6bd585b4dd2b
taa00008 <taa00008@mix.wvu.edu>
2024-03-03 16:20:47 -0500

    Update App.js
    updated for card.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import './Navbar.css';
 
 function App() {
+  const cardContent = (
+    <div>
+      <p>Hi I am Tyler Ashmore</p>
+    </div>
+  )
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800365874</p>
       <p>Hi I am Tyler Ashmore</p>
+      
+      <div className="card-container">
+        <Card title="Card 01" content={cardContent} />
+        <Card title="Card 02" content={cardContent} />
+        <Card title="Card 03" content={cardContent} />
+      </div>
     </div>
   );
 }
